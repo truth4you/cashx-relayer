bb08380db7a93c096d1699e8eaacf2efa4d6aa4f
webdev <liuzhemin0305@gmail.com>
2022-06-09 19:09:03 +0900

    multicall

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat")
+const { deploy, deployProxy, upgradeProxy } = require('./utils')
+const genContract = require('circomlib/src/mimcsponge_gencontract.js')
+
+async function main() {
+  const [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+
+  console.log("Deploying the contracts with %s on %s",owner.address,network.name)
+  const bytecode = genContract.createCode("mimcsponge",220)
+  const factory = await ethers.getContractFactory([
+      "function MiMCSponge(uint256, uint256) public view returns (uint256, uint256)"
+  ], bytecode)
+  const Hasher = await factory.deploy()
+  await Hasher.deployed()
+  const Verifier = await deploy("Verifier")
+  await deploy("CashX", Verifier.address, Hasher.address, ethers.utils.parseEther("0.1"), 20, ethers.utils.getAddress("0x0000000000000000000000000000000000000000"))
+  await deploy("Multicall")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
